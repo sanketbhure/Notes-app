2442601af8be666b1af850a6dc96e79275bf6378
sanketbhure <sanketbhure1306@gmail.com>
2024-03-15 13:48:33 +0530

    route added

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import Home from "./views/Home/Home";
+import NewNote from "./views/NewNote/NewNote";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/new",
+    element: <NewNote />
+  }
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ client/src/views/NewNote/NewNote.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./NewNote.css"
+
+function NewNote() {
+  return (
+    <div>NewNote</div>
+  )
+}
+
+export default NewNote
